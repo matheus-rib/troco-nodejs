cfe64f27fde713d527fb6ef6508d3d3506f34653
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2019-07-07 21:41:20 -0300

    Adicionada funcao calcularQuantidadeNotas e testes

--- console/troco.js
+++ console/troco.js
@@ -1,6 +1,8 @@
 // Você pode utilizar este arquivo para testar no console o retorno da função.
-// Basta executar node console/troco.js
+// Basta executar "node console/troco.js" passando como argumento um numero
+// Ex: node console/troco.js 188.86
 
-const Troco = require('../src/Troco')
+const Troco = require('../src/Troco');
 
-console.log(Troco.getQtdeNotas())
+quantidadeReais = process.argv[2] || 0;
+console.log(Troco.getQtdeNotas(quantidadeReais));
